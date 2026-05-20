9f1ee35f0726908e93ece133e6c6a286f4186590
IslaChaos13 <t.fegurgur13@gmail.com>
2026-05-20 16:48:42 -0500

    broke the api fetching, want to structure it better

--- scripts/sfetch.js
+++ scripts/sfetch.js
@@ -1,6 +1,50 @@
 let url = 'https://api.tvmaze.com/';
 
 const showDetails = document.querySelector('#showDetails');
+const showPeople = document.querySelector('#showPeople');
+const currentPage = window.location.pathname;
+
+async function fetchData(endPoint) {
+    const response = await fetch (`${url}/${endPoint}`)
+    .then (res => res.json())
+}
+
+async function loadShows(id) {
+    if (id) {
+        const post = await fetchData(`${url}/shows/${id}`)
+
+        console.log(`\nPOST ${post.id}`)
+        console.log(`Title: ${post.title}`)
+        console.log(`Body: ${post.body}\n`)
+        return
+    }
+
+async function loadData() {
+    const showIds = [1,2,3,4,5,6,7,8,9];
+    const peopleIds = [1,2,3,4,5];
+
+    const [shows, people] = await Promise.all([
+        Promise.all(
+            showIds.map(id =>
+                fetch(`https://api.tvmaze.com/shows/${id}`)
+                    .then(res => res.json())
+            )
+        ),
+        Promise.all(
+            peopleIds.map(id =>
+                fetch(`https://api.tvmaze.com/people/${id}`)
+                    .then(res => res.json())
+            )
+        )
+    ]);
+
+
+
+    if (currentPage.includes("shows.html")){
+        displayShows(shows)
+    } else (currentPage.includes(".people.html"))
+        displayPeople(people)
+}
 
 function displayShows(shows) {
     showDetails.innerHTML = "";
@@ -12,29 +56,28 @@ function displayShows(shows) {
         card.innerHTML = `
             <h2>${show.name}</h2>
             <img src="${show.image?.medium}" alt="${show.name}">
-            <p><strong>Genres:</strong> ${show.genres.join(", ")}</p>
-            <p>${show.summary}</p>
-
         `;
 
         showDetails.appendChild(card);
     });
 };
 
-async function loadShows() {
-    const ids = [1,3,4,13,17];
+function displayPeople(people) {
+    showDetails.innerHTML = "";
 
-    const shows = await Promise.all(
-        ids.map(id =>
-            fetch(`https://api.tvmaze.com/shows/${id}`)
-                .then(res => res.json())
-        )
-    );
+    people.forEach(people => {
+        const card = document.createElement('div');
+        card.classList.add('show-card');
 
-    displayShows(shows);
-}
+        card.innerHTML = `
+            <h2>${people.name}</h2>
+            <img src="${people.image?.medium}" alt="${people.name}">
+            <p> Birthdate: ${people.birthday}
+        `;
 
+        showDetails.appendChild(card);
+    });
+};
 
-loadShows();
-
+loadData();
 
